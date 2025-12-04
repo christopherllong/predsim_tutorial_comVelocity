e7c33dc538ad8979d548540a9ffd177ca1ae5672
Christopher Long <119699392+christopherllong@users.noreply.github.com>
2025-12-04 16:24:34 -0700

    Re-index amputee model settings in getSettings function
    Updated settings structure for amputee models by re-indexing.

--- settings.py
+++ settings.py
@@ -1,34 +1,28 @@
 def getSettings():
         
     settings = {        
-        # Baseline
-        '0': {
-            'model': 'Hamner_modified',
-            'targetSpeed': 1.33,
-            'N': 25},
-        
         # Amputee model. COM velocity weight at zero
-        '1': {
+        '0': {
             'model': 'Hamner_modified_amp',
             'targetSpeed': 1.33,
             'N': 25,
             'comVelocityTerm': 0},
       
         # Amputee model. COM velocity weight to 1000 (default)
-          '2': {
+          '1': {
               'model': 'Hamner_modified_amp',
               'targetSpeed': 1.33,
               'N': 25},
         
         # Amputee model. COM velocity weight to 50000
-        '3': {
+        '2': {
             'model': 'Hamner_modified_amp',
             'targetSpeed': 1.33,
             'N': 25,
             'comVelocityTerm': 50000},
       
         # Amputee model. COM velocity weight to 1000000
-        '4': {
+        '3': {
             'model': 'Hamner_modified_amp',
             'targetSpeed': 1.33,
             'N': 25,
